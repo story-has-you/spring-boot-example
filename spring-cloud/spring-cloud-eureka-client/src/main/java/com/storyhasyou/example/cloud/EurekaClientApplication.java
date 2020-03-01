78d941100dd0fa68a1a7044895fc5913e6208e38
fangxi <512618664@qq.com>
2020-03-01 17:46:30 +0800

    spring-cloud-eureka-client

---
+++ spring-cloud/spring-cloud-eureka-client/src/main/java/com/storyhasyou/example/cloud/EurekaClientApplication.java
@@ -0,0 +1,19 @@
+package com.storyhasyou.example.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author fangxi
+ * @date 2020/3/1
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
