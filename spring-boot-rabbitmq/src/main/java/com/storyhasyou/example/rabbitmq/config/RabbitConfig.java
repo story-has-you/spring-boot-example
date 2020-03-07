d4434766619e3903419b107a44139d2d25992ab5
fangxi <loveinori@qq.com>
2020-03-07 14:18:54 +0800

    RabbitMQ延迟消息

--- spring-boot-rabbitmq/src/main/java/com/storyhasyou/example/rabbitmq/config/RabbitConfig.java
+++ spring-boot-rabbitmq/src/main/java/com/storyhasyou/example/rabbitmq/config/RabbitConfig.java
@@ -2,6 +2,7 @@ package com.storyhasyou.example.rabbitmq.config;
 
 import com.storyhasyou.example.rabbitmq.constants.MqConstant;
 import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
@@ -19,24 +20,16 @@ import java.util.HashMap;
 public class RabbitConfig {
 
     /**
-     * 创建死信队列
+     * 创建死信队列(缓冲队列)
      */
     @Bean
-    public Queue directQueue() {
+    public Queue deadLetterQueue() {
         HashMap<String, Object> map = new HashMap<>(2);
         // 出现dead letter之后将dead letter重新发送到指定exchange
         map.put("x-dead-letter-exchange", MqConstant.DEAD_EXCHANGE);
         // 出现dead letter之后将dead letter重新按照指定的routing-key发送
         map.put("x-dead-letter-routing-key", MqConstant.DELAY_ROUTING_KEY);
         return QueueBuilder.nonDurable(MqConstant.DEAD_QUEUE).withArguments(map).build();
-    }
-
-    /**
-     * 创建需要被消费的队列
-     */
-    @Bean
-    public Queue redirectQueue() {
-        return QueueBuilder.nonDurable(MqConstant.DELAY_QUEUE).build();
     }
 
     /**
@@ -52,15 +45,24 @@ public class RabbitConfig {
      * 将 死信队列 与 死信队列的交换机 绑定
      */
     @Bean
-    public Binding deadLetterBinding() {
-        return new Binding(MqConstant.DEAD_QUEUE, Binding.DestinationType.QUEUE, MqConstant.DEAD_EXCHANGE, MqConstant.DEAD_ROUTING_KEY, null);
+    public Binding deadLetterBinding(Queue deadLetterQueue, Exchange deadLetterExchange) {
+        return BindingBuilder.bind(deadLetterQueue).to(deadLetterExchange).with(MqConstant.DEAD_ROUTING_KEY).noargs();
     }
+
+    /**
+     * 创建需要被消费的队列
+     */
+    @Bean
+    public Queue redirectQueue() {
+        return QueueBuilder.nonDurable(MqConstant.DELAY_QUEUE).build();
+    }
+
 
     /**
      * 将需要消费的队列 与 死信队列和死信队列的交换机绑定
      */
     @Bean
-    public Binding redirectBinding() {
-        return new Binding(MqConstant.DELAY_QUEUE, Binding.DestinationType.QUEUE, MqConstant.DEAD_EXCHANGE, MqConstant.DELAY_ROUTING_KEY, null);
+    public Binding redirectBinding(Queue redirectQueue, Exchange deadLetterExchange) {
+        return BindingBuilder.bind(redirectQueue).to(deadLetterExchange).with(MqConstant.DELAY_ROUTING_KEY).noargs();
     }
 }
