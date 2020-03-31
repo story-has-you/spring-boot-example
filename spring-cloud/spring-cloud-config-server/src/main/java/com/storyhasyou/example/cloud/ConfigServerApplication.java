9cd245eae31e644ee74dddb5bfe4a1cdb8495da7
fangxi <loveinori@qq.com>
2020-03-31 16:17:43 +0800

    spring-cloud-config-server

---
+++ spring-cloud/spring-cloud-config-server/src/main/java/com/storyhasyou/example/cloud/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.storyhasyou.example.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author fangxi
+ * @date 2020/3/31
+ * @since 1.0.0
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
