678f02d2bd7369a27dda2f36d3088bccc5e7edbb
fangxi <loveinori@qq.com>
2020-04-01 13:27:48 +0800

    spring-cloud-gateway基本使用

--- spring-cloud-example/spring-cloud-gateway/src/main/java/com/storyhasyou/example/cloud/RoutersConfig.java
+++ spring-cloud-example/spring-cloud-gateway/src/main/java/com/storyhasyou/example/cloud/RoutersConfig.java
@@ -1,6 +1,11 @@
 package com.storyhasyou.example.cloud;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 
 /**
  * @author fangxi
@@ -10,6 +15,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RoutersConfig {
 
+    @Bean
+    @Order
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/java/**")
+                        .and().method(HttpMethod.GET)
+                        // header中如果有token这个name则放行
+                        .and().header("token")
+                        .filters(f -> f.stripPrefix(1).addResponseHeader("java-param", "gateway"))
+                        .uri("lb://spring-cloud-openfeign")
+                ).build();
+    }
 
 
 }
