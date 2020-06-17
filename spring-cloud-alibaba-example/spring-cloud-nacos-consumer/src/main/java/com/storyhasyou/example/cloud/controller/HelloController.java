e275ab1f53de097d54de83eb8b9dfc6bdf17e770
fangxi <loveinori@qq.com>
2020-06-17 16:08:09 +0800

    Alibaba Nacos 分布式配置中心

--- spring-cloud-alibaba-example/spring-cloud-nacos-consumer/src/main/java/com/storyhasyou/example/cloud/controller/HelloController.java
+++ spring-cloud-alibaba-example/spring-cloud-nacos-consumer/src/main/java/com/storyhasyou/example/cloud/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.storyhasyou.example.cloud.controller;
 
 import com.storyhasyou.example.cloud.feign.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 /**
  * @author fangxi created by 2020/6/17
  */
+@RefreshScope
 @RestController
 public class HelloController {
 
@@ -18,6 +21,9 @@ public class HelloController {
 
     @Autowired
     private EchoService echoService;
+
+    @Value("${user.name}")
+    private String username;
 
     @GetMapping(value = "/echo/{str}")
     public String echo(@PathVariable String str) {
@@ -29,5 +35,10 @@ public class HelloController {
     public String echoFeign(@PathVariable String str) {
         // 使用服务名请求服务提供者
         return echoService.echo(str);
+    }
+
+    @GetMapping(value = "/config")
+    public String config() {
+        return echoService.echo(username);
     }
 }
